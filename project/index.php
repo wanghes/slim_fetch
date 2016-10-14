c8af61193c8811b66e2b2c40b937525147b912ed
wanghaisong <wanghaisong@huoyunren.com>
2016-10-14 18:02:40 +0800

    ads

--- project/index.php
+++ project/index.php
@@ -42,8 +42,7 @@ $container['logger'] = function($c) {
 };
 $container['db'] = function ($c) {
     $db = $c['settings']['db'];
-    $pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'],
-        $db['user'], $db['pass']);
+    $pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'], $db['user'], $db['pass']);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     $pdo->exec("SET CHARACTER SET utf8");
@@ -71,6 +70,13 @@ $app->get('/',function(Request $request, Response $response,$args){
     foreach ($headers as $name => $values) {
         echo $name . ": " . implode(", ", $values);
     }
+});
+
+$app->post('/user/login',function(Request $request, Response $response){
+    //$isXHR = $request->isXhr();
+
+    $parsedBody = $request->getParsedBody();
+    $response->withJson($parsedBody);
 });
 
 $app->post('/user',function(Request $request, Response $response){
